5fbdfb598e20d7206fb1253cb49a593903ea607f
178inaba <178inaba@users.noreply.github.com>
2016-05-24 19:34:39 +0900

    add TestAdvanceRunes

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestGetNextRune(t *testing.T) {
 	var got, want rune
@@ -20,6 +23,34 @@ func TestGetNextRune(t *testing.T) {
 	got = getNextRune('Z')
 	want = '0'
 	if got != want {
+		t.Errorf("error: got rune %s, want %s.", string(got), string(want))
+	}
+}
+
+func TestAdvanceRunes(t *testing.T) {
+	var got, want []rune
+
+	got = advanceRunes([]rune{'Z'})
+	want = []rune{'0', '0'}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("error: got rune %s, want %s.", string(got), string(want))
+	}
+
+	got = advanceRunes([]rune{'0', '0'})
+	want = []rune{'0', '1'}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("error: got rune %s, want %s.", string(got), string(want))
+	}
+
+	got = advanceRunes([]rune{'9', '9'})
+	want = []rune{'9', 'a'}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("error: got rune %s, want %s.", string(got), string(want))
+	}
+
+	got = advanceRunes([]rune{'Z', 'Z'})
+	want = []rune{'0', '0', '0'}
+	if !reflect.DeepEqual(got, want) {
 		t.Errorf("error: got rune %s, want %s.", string(got), string(want))
 	}
 }
