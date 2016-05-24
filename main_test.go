49ea1625a3a9c5d7054a6e4d04d26a3fa3591949
178inaba <178inaba@users.noreply.github.com>
2016-05-24 18:38:07 +0900

    fix test

--- main_test.go
+++ main_test.go
@@ -5,8 +5,20 @@ import "testing"
 func TestGetNextRune(t *testing.T) {
 	var got, want rune
 
-	got = getNextRune('a')
-	want = 'b'
+	got = getNextRune('9')
+	want = 'a'
+	if got != want {
+		t.Errorf("error: got rune %s, want %s.", string(got), string(want))
+	}
+
+	got = getNextRune('z')
+	want = 'A'
+	if got != want {
+		t.Errorf("error: got rune %s, want %s.", string(got), string(want))
+	}
+
+	got = getNextRune('Z')
+	want = '0'
 	if got != want {
 		t.Errorf("error: got rune %s, want %s.", string(got), string(want))
 	}
